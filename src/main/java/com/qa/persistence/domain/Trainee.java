2c5c06d5f787b50642cdb99e5f3ac6643d9011ba
washworth01 <washworth01@gmail.com>
2019-02-01 10:28:20 +0000

    Domain Noun classes created with constructors, attributes, setters and getters.

---
+++ src/main/java/com/qa/persistence/domain/Trainee.java
@@ -0,0 +1,45 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trainee 
+{
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long traineeID;
+	
+	private String traineeName;
+	
+	
+	public Trainee()
+	{
+		
+	}
+	
+	public Trainee(String traineeName)
+	{
+		this.traineeName = traineeName;
+	}
+
+	public Long getTraineeID() {
+		return traineeID;
+	}
+
+	public void setTraineeID(Long traineeID) {
+		this.traineeID = traineeID;
+	}
+
+	public String getTraineeName() {
+		return traineeName;
+	}
+
+	public void setTraineeName(String traineeName) {
+		this.traineeName = traineeName;
+	}
+	
+	
+}
